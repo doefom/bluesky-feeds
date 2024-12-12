7a18c5a25ff1990c4acc734c9e8edcb571895316
doefom <dom.mueller94@gmail.com>
2024-12-12 18:37:07 +0100

    Change back to BskyFeedGenerator

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use App\Http\Controllers\FeedController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('/.well-known/did.json', [FeedController::class, 'getDidDocument']);
 
+Route::get('/getFeedSkeleton', [FeedController::class, 'getFeedSkeleton']);
 Route::get('/xrpc/app.bsky.feed.getFeedSkeleton', [FeedController::class, 'getFeedSkeleton']);
