d9d6a461add76b7adc95a71bbbd746fc58e55ea3
doefom <dom.mueller94@gmail.com>
2024-12-12 18:45:45 +0100

    Remove unused routes

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/.well-known/did.json', [FeedController::class, 'getDidDocument']);
 
-Route::get('/', [FeedController::class, 'getFeedSkeleton']);
-Route::get('/getFeedSkeleton', [FeedController::class, 'getFeedSkeleton']);
 Route::get('/xrpc/app.bsky.feed.getFeedSkeleton', [FeedController::class, 'getFeedSkeleton']);
